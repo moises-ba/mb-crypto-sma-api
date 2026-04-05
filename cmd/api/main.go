1f41f596b575b4d3ab6834bd0476ad9b103f07ca
Moisés Almeida <moises@MacBook-Pro-de-Moises.local>
2026-04-05 12:46:21 -0300

    controller implementation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -26,7 +26,6 @@ func main() {
 		})
 
 		v1.GET("/lastclosedprices/:days/:dt_reference", lastClosedPricesCtrl.ListLastClosedPrices) //escolhido path para porque os parametros sao obrigatorios data no formato YYYY-MM-DD
-
 	}
 
 	r.Run(":8080")
